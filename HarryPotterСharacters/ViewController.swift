49f521daabf99fad9a6946ccd8ddc3746a2e6563
Oksenoyt <a.oksenoyt@gmail.com>
2022-08-09 14:47:51 +0400

    Initial Commit

---
+++ HarryPotterСharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterСharacters
+//
+//  Created by Elenka on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
