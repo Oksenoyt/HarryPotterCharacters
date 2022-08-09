61138b894a7e5ad9d19a9581a420ba6d9b655650
Oksenoyt <a.oksenoyt@gmail.com>
2022-08-09 16:48:58 +0400

    get data

--- HarryPotterСharacters/ViewController.swift
+++ HarryPotterСharacters/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    
+    let link = "http://hp-api.herokuapp.com/api/characters"
+    
 }
 
+extension ViewController {
+    private func fetchCharacters() {
+        guard let url = URL(string: link) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            print(response)
+            
+            do {
+                let characters = try JSONDecoder().decode([Character].self, from: data)
+                print(characters)
+            } catch let error {
+                print(error)
+            }
+            
+            
+//            guard let image = UIImage(data: data) else { return }
+//            DispatchQueue.main.async {
+//                self?.imageView.image = image
+//            }
+        }.resume()
+    }
+}
---
+++ HarryPotterСharacters/Сharacter.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  HarryPotterСharacters
+//
+//  Created by Elenka on 09.08.2022.
+//
+struct Character: Decodable {
+    
+    let name: String
+    let wizard: Bool
+    let house: String
+    let wand: Wand
+    let hogwartsStudent: Bool
+    let image: String
+    let actor: String
+}
+
+struct Wand: Decodable {
+    let wood: String
+    let core: String
+//    let length: Int?
+}
