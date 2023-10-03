63d68e446dac4eb35ef54958fb164ddd6582930e
Elenka <kholodilinaes@tech.bcs.ru>
2023-10-03 11:29:45 -0700

    added layout for spellCell

---
+++ HarryPotterСharacters/ViewControllers/SpellsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SpellsTableViewCell.swift
+//  HarryPotterСharacters
+//
+//  Created by Oksenoyt on 03.10.2023.
+//
+
+import UIKit
+
+class SpellsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
